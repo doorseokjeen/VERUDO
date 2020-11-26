6b939c13c27c4620b63baa08cf577b5562441a41
doorseokjeen <74698088+doorseokjeen@users.noreply.github.com>
2020-11-26 11:37:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -23,28 +23,7 @@ var locationOfclickedTime;
 
 var originY = window.innerHeight,
     originX = window.innerWidth;
-
-var touches = evt.changedTouches;
-for (var i = 0; i < touches.length; i++) {
-    var color = colorForTouch(touches[i]);
-    var idx = ongoingTouchIndexById(touches[i].identifier);
-
-    if (idx >= 0) {
-      log("continuing touch "+idx);
-      ctx.beginPath();
-      log("ctx.moveTo(" + ongoingTouches[idx].pageX + ", " + ongoingTouches[idx].pageY + ");");
-      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-      log("ctx.lineTo(" + touches[i].pageX + ", " + touches[i].pageY + ");");
-      ctx.lineTo(touches[i].pageX, touches[i].pageY);
-      ctx.lineWidth = 4;
-      ctx.strokeStyle = color;
-      ctx.stroke();
-
-      ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-      log(".");
-    } else {
-      log("can't figure out which touch to continue");
-	    
+    
 var mouse;
 
 var startTime = new Date().getTime(); // Get start time for animating
